cfbdbaa6db8fcac34ab3a22dd326bf6b3d2a735c
Roman2024 <105998414+roman-vishinskij@users.noreply.github.com>
2024-07-21 13:38:37 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,7 @@
+my_string = input("Ввидите имя:")
+print( "Привет:", my_string.upper())
+print( "Привет:", my_string.lower())
+print( "Привет:", my_string.replace(' ',''))
+print( "Привет:", my_string [0])
+print( "Привет:", my_string [-1])
+
